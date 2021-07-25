dfd6c8a9cb6c9d205670069cb109041ae776830b
1jokras <derechtejokras@gmail.com>
2021-07-25 18:18:26 +0200

    fix Falsche Turm Nummern

--- godot/Burg/Hof/Scripts/background_music_hof.gd
+++ godot/Burg/Hof/Scripts/background_music_hof.gd
@@ -1,6 +1,7 @@
 extends AudioStreamPlayer
 
 func _ready():
+	self.play()
 	$"../Test/Level_test".connect("body_entered", self, "play_musik")
 	$"../Test/Level_test2".connect("body_entered", self, "play_musik")
 	
@@ -8,9 +9,9 @@ func _process(delta):
 	if 	$"/root/Level/T1/Level_test" != null:
 		if $"/root/Level/T1/Level_test".is_connected("body_entered", self, "stop_musik") == false:
 			$"/root/Level/T1/Level_test".connect("body_entered", self, "stop_musik")
-	if 	$"/root/Level/T2/Level_test" != null:
-		if $"/root/Level/T2/Level_test".is_connected("body_entered", self, "stop_musik") == false:
-			$"/root/Level/T2/Level_test".connect("body_entered", self, "stop_musik")
+	if 	$"/root/Level/T8/Level_test2" != null:
+		if $"/root/Level/T8/Level_test2".is_connected("body_entered", self, "stop_musik") == false:
+			$"/root/Level/T8/Level_test2".connect("body_entered", self, "stop_musik")
 
 func play_musik(_body):
 	if self.playing == false:
